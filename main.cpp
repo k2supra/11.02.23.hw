360fa456946a677ea0ee747eafdd57ef2cad5620
k2supra <andrijkorolevic@gmail.com>
2023-02-13 10:54:29 +0200

    .

--- main.cpp
+++ main.cpp
@@ -3,6 +3,28 @@
 
 using namespace std;
 
-int main() {
+template<typename T>
+T* create_array(int size)
+{
+	T* array = new T[size];
+	int sum = 0;
+	int odd = 1;
+	for (size_t i = 0; i < size; i++)
+	{
+		array[i] = T(rand() % 50);
+		cout << array[i] << '\t';
+		sum += array[i];
+		odd *= array[i];
+	}
+	cout << "\nSum = " << sum;
+	cout << "\nOdd = " << odd;
+	return 0;
+}
 
+int main() {
+	srand(time(0));
+	int size;
+	cout << "Enter a size -->";
+	cin >> size;
+	int* array = create_array<int>(size);
 }
